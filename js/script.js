60e55f2ea27cde8ca26b5e3fd737457096c680e5
Tsun707 <tsun24@id.iit.edu>
2023-11-29 11:48:41 -0800

    map loading

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
  function initMap(){
+let map;
 
+async function initMap() {
+  const position = { lat: 43.85756120394474, lng: 18.41114065957382 };
+  const { Map } = await google.maps.importLibrary("maps");
+
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
   window.addEventListener('load', init);
