d08e67accc66ef47bf7bb3f61024478b11f0d665
Tsun707 <tsun24@id.iit.edu>
2023-11-29 11:13:09 -0800

    map updating

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
  function initMap(){
-	  let sarajevo = {lat:43.858551426878236, lng:18.412856928904617};
-	  let map = new google.maps.Map(
-	  document.getElementById('map'),{zoom: 4, center: sarajevo}
-	  );
-	}
-  
-  <script defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+
+  window.addEventListener('load', init);
