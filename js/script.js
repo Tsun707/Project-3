0aaf978a33e5d8f1e4c6aa957737b33b56bc9a1e
Tsun707 <tsun24@id.iit.edu>
2023-12-01 15:52:30 -0800

    map update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
  function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat:43.85756120394474, lng:18.41114065957382},
-      zoom: 8
-    });
+  let map;
+
+async function initMap() {
+  const position = { lat: 43.85756120394474, lng: -18.41114065957382 };
+
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 16,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Sarajevo",
+  });
+}
+
+initMap();
+
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
