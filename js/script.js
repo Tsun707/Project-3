77d67a74bc50469d5ca5b43d6d52ecb0f4ce4af3
Tsun707 <tsun24@id.iit.edu>
2023-12-01 14:37:38 -0800

    map loading update

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
-let map;
-
  function initMap(){
-	 const position = {lat:43.858551426878236, lng:18.412856928904617};
-	 const{map} = async google.maps.importLibrary('maps');
-	 const{AdvancedMarkerView} = async google.maps.importLibrary('marker');
-	 map = new map(document.getElementById("map"),{
-		 zoom: 4,
-		 center: position,		 
-	 });
-	 
-	 const marker = new AdvancedMarkerView({
-		 map: map,
-		 position: position,
-		 title: "Sarajevo",
-	 });
- }
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat:43.85756120394474, lng:18.41114065957382},
+      zoom: 4
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
