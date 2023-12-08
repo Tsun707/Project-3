fae98cc6b05b4c34203ceef9cf0f129d1fc1dd8c
Tsun707 <tsun24@id.iit.edu>
2023-12-07 20:49:11 -0800

    Song Updating

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
- function initMap(){
-  let map;
-
-async function initMap() {
-  const position = { lat: 43.85756120394474, lng: -18.41114065957382 };
-
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-
-  map = new Map(document.getElementById("map"), {
-    zoom: 16,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+  function initMap(){
+   var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 43.85756120394474, lng: 18.41114065957382},
+      zoom: 7
+    });
+	
+	var criticalcity = [
+  [{lat: 43.85756120394474, lng: 18.41114065957382},  "Siege of Sarajevo-Sarajevo"],
+  [{lat: 44.771982343048585, lng: 17.192773920708355},  "Republika Srpska-Banja Luka"],
+  [{lat:  43.34404101163839, lng: 17.809738305686924},  "Federation of Bosnia and Herzegovina-Mostar"],
+  ];
+  
+  var infoWindow = new google.map.InfoWindow();
+  criticalcity.forEach(([position, title], i) => {
+	  var marker = new google.map.Marker({
+		  position,
+		  map,
+		  title: '${i + 1}. ${title}',
+		  label: '${i + 1}',
+		  optimized: false,
+	  });
+	  
+	  marker.addListener("click", () =>{
+		  infoWindow.close();
+		  infoWindow.setContent(marker .getTitle());
+		  infoWindow.open(marker.getMap(), marker);
+	  });
   });
+  }
+ 
+  
 
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: { lat: 43.85756120394474, lng: -18.41114065957382 },
-    title: "Sarajevo",
-  });
-}
 
-initMap();
-
-}
+google.maps.event.addDomListener(window, 'load', initMap);
 
